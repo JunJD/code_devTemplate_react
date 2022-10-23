67fbc8c85c70cd2060affae749fe4e91e748f8e4
dingjunjie <dingjunjie@ypshengxian.com>
2022-10-23 18:36:02 +0800

    feat（模糊搜索用户）

--- src/page/AuthorityCenter/UserAdmin/index.tsx
+++ src/page/AuthorityCenter/UserAdmin/index.tsx
@@ -1,31 +1,27 @@
 import BaseTable, { IColumn } from '@src/page/components/baseTable';
 import _columns from './_colums';
-import React, { useMemo, useState } from 'react';
-import { Col, Input, Popover, Row, Select, Space } from 'antd';
+import React, { ChangeEvent, useMemo, useState } from 'react';
+import { Button, Col, Input, Row, Select, Space, Tabs } from 'antd';
 import { useDynamicColumns } from '@src/hooks/useDynamicColumns';
-import { SearchOutlined, HolderOutlined } from '@ant-design/icons';
+import { SearchOutlined, HolderOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { debounce } from 'lodash';
+
+const scrollX = document.body.clientWidth - 220 - 100
+
 const UserAdim: React.FC = () => {
   const { columns: dynamicColumns, CtrlButton } = useDynamicColumns(_columns )
-
   const [ query, setQuery ] = useState( {
     url: 'admin/get',
-    params: { }
+    params: { fuzzy: "" }
   } )
 
-  // const handleSearch=(params)=>{
-  //   setQuery( { ...query, params: { ...params } } )
-  // }
-  // const handleEcho=()=>{
-  //   setQuery( { ...query, params: { ...query.params } } )
-  // }
+  const handleSearch=()=>{
+    return debounce((e:ChangeEvent<HTMLInputElement>)=>{
+      const fuzzy = e.target.value
+      setQuery( { ...query, params: { fuzzy } } )
+    },500)
+  }
 
-
-  const content = (
-    <div>
-      <p>Content</p>
-      <p>Content</p>
-    </div>
-  );
   const finallyColumns = useMemo( () => {
     return [
         ...dynamicColumns,
@@ -43,17 +39,25 @@ const UserAdim: React.FC = () => {
     ]
   },[ dynamicColumns ] )
 
-  return (
+  const content = useMemo(()=>(
     <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
       <Row>
         <Col span={2}>
           {CtrlButton}
         </Col>
         <Col span={6} offset={12}>
-          <Input bordered={false} className='myCard' allowClear placeholder="Search" prefix={<SearchOutlined />} />
+          <Input 
+          size="large"
+          bordered={false} 
+          className='myCard' 
+          allowClear 
+          placeholder="Search" 
+          prefix={<SearchOutlined />} 
+          onChange={handleSearch()} />
         </Col>
         <Col span={3} offset={1} >
           <Select
+            size="large"
             bordered={false}
             className='myCard'
             showSearch
@@ -66,10 +70,44 @@ const UserAdim: React.FC = () => {
       </Row>
       <Row>
         <Col span={24}>
-          <BaseTable query={query} columns={finallyColumns} scroll={{ x: 1200,y: 400 }} />
+          <BaseTable query={query} columns={finallyColumns} scroll={{ x: scrollX,y: 600 }} />
         </Col>
       </Row>
-    </Space>  
+    </Space>
+  ),[ query.params, CtrlButton, finallyColumns, scrollX ])
+
+  return (
+    <div>
+      <Row>
+        <Col span={4}>
+          <span style={{fontWeight:700,fontSize:"32px"}} >User&Admin</span>
+        </Col>
+      <Col span={6} offset={14}>
+        <Button type="primary" icon={<PlusCircleOutlined />} size="large">
+          Add newAdmin
+        </Button>
+      </Col>
+      </Row>
+
+      <div className='pdb-16'></div>
+
+      <Tabs
+        defaultActiveKey="1"
+        onChange={()=>{}}
+        items={[
+          {
+            label: <h2>Admin</h2>,
+            key: '1',
+            children: content,
+          },
+          {
+            label: <h2>User</h2>,
+            key: '2',
+            children: `Content of Tab Pane 2`,
+          }
+        ]}
+      />
+    </div>
   )
 };
 
